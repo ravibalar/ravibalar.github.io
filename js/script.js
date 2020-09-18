170976d15078d4e291f1288d133a27ed9cf78d2b
ravibalar <balar025@gmail.com>
2020-09-18 23:15:53 +1000

    new site commit 20200918
    new site commit 20200918

---
+++ js/script.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+  $('.skill-slider').slick({
+    infinite: true,
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 3,
+    slidesToScroll: 1,
+
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+});
